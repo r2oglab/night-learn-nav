18ec90b91d68b929852c19b0c7a591c4ebb9c546
r2oglab <ryan.godine@gmail.com>
2026-08-09 03:57:38 -0300

    Restaura caracteres < perdidos nas declaracoes genericas de types.ts

--- src/integrations/supabase/types.ts
+++ src/integrations/supabase/types.ts
@@ -202,7 +202,7 @@ type DatabaseWithoutInternals = Omit<Database, "__InternalSupabase">
 
 type DefaultSchema = DatabaseWithoutInternals[Extract<keyof Database, "public">]
 
-export type Tables
+export type Tables<
   DefaultSchemaTableNameOrOptions extends
     | keyof (DefaultSchema["Tables"] & DefaultSchema["Views"])
     | { schema: keyof DatabaseWithoutInternals },
@@ -231,7 +231,7 @@ export type Tables
       : never
     : never
 
-export type TablesInsert
+export type TablesInsert<
   DefaultSchemaTableNameOrOptions extends
     | keyof DefaultSchema["Tables"]
     | { schema: keyof DatabaseWithoutInternals },
@@ -256,7 +256,7 @@ export type TablesInsert
       : never
     : never
 
-export type TablesUpdate
+export type TablesUpdate<
   DefaultSchemaTableNameOrOptions extends
     | keyof DefaultSchema["Tables"]
     | { schema: keyof DatabaseWithoutInternals },
@@ -281,7 +281,7 @@ export type TablesUpdate
       : never
     : never
 
-export type Enums
+export type Enums<
   DefaultSchemaEnumNameOrOptions extends
     | keyof DefaultSchema["Enums"]
     | { schema: keyof DatabaseWithoutInternals },
@@ -298,7 +298,7 @@ export type Enums
     ? DefaultSchema["Enums"][DefaultSchemaEnumNameOrOptions]
     : never
 
-export type CompositeTypes
+export type CompositeTypes<
   PublicCompositeTypeNameOrOptions extends
     | keyof DefaultSchema["CompositeTypes"]
     | { schema: keyof DatabaseWithoutInternals },
